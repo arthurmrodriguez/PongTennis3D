31fc2c3b375917810168d668102e081e4def3af1
Adrián Morente <adrian95morente@gmail.com>
2018-05-04 20:18:00 +0200

    :construction: adding basic files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
     context: __dirname + "/src",
-    entry: "./index",
+    entry: "./js/index",
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
