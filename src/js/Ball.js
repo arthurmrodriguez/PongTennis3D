e71d38bc0de494da5eca51e6fd956cf582a88c66
Adrián Morente <adrian95morente@gmail.com>
2018-05-21 20:19:54 +0200

    some comments + collision events (to implement)

--- src/js/Ball.js
+++ src/js/Ball.js
@@ -5,16 +5,25 @@ import ballTexture from '../img/tennisBall.png';
 
 export default class Ball extends THREE.Object3D {
     
-    constructor(){
+    /**
+     * Class constructor with no parameters
+     */
+    constructor(color = Config.ball.color){
         super();
 
-        // Parameters
-        this.mass = Config.ball.mass;
+        // Parameters for a better looking
         this.radius = Config.ball.radius;
         this.numSegments = Config.ball.numSegments;
-        this.color = Config.ball.color;
+        this.color = color;
+        
+        // Mass needs to be higher than 0 in order to be affected by world gravity
+        this.mass = Config.ball.mass;
 
-        // 1 - THREE object
+        // Parameters needed to control the development of the game
+        this.lastPlayerCollided = null;
+        this.lastHalfOfCourtCollided = null;
+
+        // 1 - THREE object - creates geometry and loads a tennis ball texture
         this.geometry = new THREE.SphereGeometry(this.radius, this.numSegments, this.numSegments);
         this.material = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(ballTexture) });
         this.mesh = new THREE.Mesh(this.geometry, this.material);
@@ -22,7 +31,7 @@ export default class Ball extends THREE.Object3D {
         this.mesh.receiveShadow = true;
         this.add(this.mesh);
 
-        // 2- CANNON object
+        // 2- CANNON object - just a sphere with a contact material (so it can collide in a specific way with other bodies)
         this.sphereShape = new CANNON.Sphere(this.radius);
         this.contactMaterial = new CANNON.Material();
         this.body = new CANNON.Body({
@@ -34,9 +43,11 @@ export default class Ball extends THREE.Object3D {
         this.body.velocity.set(0,0,150);
         Config.bodyIDs.ballID = this.body.id;
 
-        // Listener event to detect collisions with other objects.
-        // Proper function will be triggered with each collision
-        // Body.id could be used for classification.
+        /**
+         * Listener event to detect collisions with other objects.
+         * Proper function will be triggered with each collision.
+         * Body.id could be used for classification.
+         */
         this.body.addEventListener("collide", this.handleCollision);
     }
 
@@ -56,27 +67,54 @@ export default class Ball extends THREE.Object3D {
     }
 
     /**
+     * This method checks every collision with every body on the world.
+     * It always has to check if a point has been scored by any player.
      * 
+     * --- RULES ABOUT SCORING ---
+     * 
+     * P1 loses a point if:
+     *      - P2 hits the ball, the ball bounces in P1's half of the
+     *        court and the play ends.
+     *      - P1 hits the ball, and then the ball bounces in P1's
+     *        half of the court.
+     *      - P1 hits the ball and it goes directly out of the court.
+     * 
+     * A play ends when the ball bounces twice with no player strikes in
+     * between; or when it goes out of the court.
      */
     handleCollision(collision){
         switch (collision.body.id) {
             case Config.bodyIDs.courtID:
-                // console.log("Colisiona con Court");
+                if(this.position.z > 0)
+                    this.lastHalfOfCourtCollided = "2";
+                else //this.body.position.z < 0
+                    this.lastHalfOfCourtCollided = "1";
+                break;
+            case Config.bodyIDs.netID:
+                break;
+            case Config.bodyIDs.racketP1ID:
+                this.lastPlayerCollided = "1";
+                break;
+            case Config.bodyIDs.racketP2ID:
+                this.lastPlayerCollided = "2";
                 break;
             default:
                 break;
         }
+        console.log('\n\n\nlast court collision: ' + this.lastHalfOfCourtCollided +
+                    '\nlast player collided: ' + this.lastPlayerCollided);
     }
 
     /**
-     * 
+     * This method is responsible of doing the clones between the collidable meshes
+     * (CANNON bodies) and the watchable ones (THREE meshes).
      */
     updateMeshPosition(){
         // Copy coordinates from Cannon.js world to Three.js'
         this.mesh.position.copy(this.body.position);
         this.mesh.quaternion.copy(this.body.quaternion);
 
-        // temporal para pruebas
+        // temporary trial - just places the ball in a playable position
         if(this.body.position.y <= -50){
             this.setPosition(0, Config.ball.bounceHeight, -100);
             this.body.velocity.set(0,0,150);
